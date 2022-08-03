c168032a39380ee46c77423037dcb76844833cf8
German <110214971+gerchenko@users.noreply.github.com>
2022-08-03 18:12:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("icon-menu").addEventListener("click", mostrar_menu);
+
+function mostrar_menu(){
+        document.getElementById("show-menu").classList.toggle('show-lateral');
+}
